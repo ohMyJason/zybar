b10ea7a1f281b2b3aee8b175f249ae326a0d7e4e
ohmyjason <719134834@qq.com>
2019-07-24 22:26:36 +0800

    init repo

---
+++ src/main/java/com/zybar/bar/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.zybar.bar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author 刘佳昇
+ * @Date 2019/7/24 22:04
+ */
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name")String name, Model model){
+
+        model.addAttribute("name",name);
+
+        return "hello";
+
+    }
+}
+
